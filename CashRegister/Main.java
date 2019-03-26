c50126da617f7e0c91c5b89934d7ac62cc9fdb5d
frielliam2019 <42677906+frielliam2019@users.noreply.github.com>
2019-03-26 08:42:28 -0700

    Add files via upload

---
+++ CashRegister/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String desired = sc.next();
+        switch (desired) {
+            case "type":
+                break;
+            case "name":
+                break;
+            case "codeNumber":
+                break;
+            case "price":
+                break;
+            case "quantity":
+                break;
+            case "tax":
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ CashRegister/Product.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+public class Product {
+    public ArrayList help = new ArrayList();
+
+    public Product(){
+
+    }
+
+    public void name(){
+
+    }
+
+    public void codeNumber(){
+
+    }
+    public void price(){
+
+    }
+
+    public void quantity(){
+
+    }
+    public void type(){
+
+    }
+    public void salesTax(){
+
+    }
+
+}
